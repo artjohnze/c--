6feb25dc7138a445385dd8d73dd6b04980887e0c
Art John-Zelle Balubal <artjohn-zelle.balubal_cyn@isu.edu.ph>
2025-11-01 22:56:56 +0800

    last

--- c.cpp
+++ c.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
-
+using namespace std;
 int main()
 {
 
     int num1, num2, num3;
 
-    std::cout << "Enter the first integer: ";
-    std::cin >> num1;
+    cout << "Enter the first integer: ";
+    cin >> num1;
 
-    std::cout << "Enter the second integer: ";
-    std::cin >> num2;
+    cout << "Enter the second integer: ";
+    cin >> num2;
 
-    std::cout << "Enter the third integer: ";
-    std::cin >> num3;
+    cout << "Enter the third integer: ";
+    cin >> num3;
 
     int sum = num1 + num2 + num3;
     int difference = num1 - num2 - num3;
@@ -27,15 +27,15 @@ int main()
     }
     else
     {
-        std::cout << "Division or modulus by zero is undefined.\n";
+        cout << "Division or modulus by zero is undefined.\n";
         return 1;
     }
 
-    std::cout << "Sum of all three numbers: " << sum << std::endl;
-    std::cout << "Difference (first - second - third): " << difference << std::endl;
-    std::cout << "Product of all three numbers: " << product << std::endl;
-    std::cout << "Division (first / (second * third)): " << division << std::endl;
-    std::cout << "Modulus ((first % second) % third): " << modulus << std::endl;
+    cout << "Sum of all three numbers: " << sum << endl;
+    cout << "Difference (first - second - third): " << difference << endl;
+    cout << "Product of all three numbers: " << product << endl;
+    cout << "Division (first / (second * third)): " << division << endl;
+    cout << "Modulus ((first % second) % third): " << modulus << endl;
 
     return 0;
 }
